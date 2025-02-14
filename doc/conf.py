8c443819dc845a0257a6fdbd7b7426f2813da3e6
AlbertEngstfeld <albert.engstfeld@gmail.com>
2025-02-14 13:27:40 +0100

    bumped version to 0.3.0

--- doc/conf.py
+++ doc/conf.py
@@ -2,7 +2,7 @@ project = 'echemdb-converters'
 copyright = '2022-2023, the echemdb-converters authors'
 author = 'the echemdb-converters authors'
 
-release = 'clean'
+release = '0.3.0'
 
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.todo", "myst_nb"]
 
